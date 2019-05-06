80e7fcb8ab8f5b3427ebdb1643c74aef72015cc1
Danny Robinson <daniel@djrobinson.me>
2019-05-05 21:58:45 -0600

    Add in static query

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,34 @@
 import React from "react"
+import { StaticQuery, graphql } from 'gatsby';
 
-export default () => <div>Hello world!</div>
+const Header = () => {
+    return (
+        <StaticQuery
+            query={graphql`
+                query {
+                    site {
+                        siteMetadata {
+                            title
+                            description
+                        }
+                    }
+                }
+            `}
+            render={data => {
+                return (
+                    <h1>{data.site.siteMetadata.title}</h1>
+                )
+            }}
+        />
+    )
+}
+
+const Layout = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
+
+export default Layout

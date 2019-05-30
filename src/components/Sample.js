17a0442eb68d06dde92583b7850310a10dfedad8
Danny Robinson <daniel@djrobinson.me>
2019-05-30 14:47:23 -0600

    Prims sample working

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -1,6 +1,7 @@
 import React from "react"
 import * as d3 from 'd3'
 
+
 class Sample extends React.Component {
     constructor(props) {
       super(props)
@@ -9,22 +10,91 @@ class Sample extends React.Component {
       }
     }
     componentDidMount() {
-        let svg = d3.select(this._rootNode),
-          width = Number(svg.style('width').slice(0, -2)),
-          height = Number(svg.style('height').slice(0, -2)),
-          innerRadius = 5,
-          outerRadius = Math.min(width, height) / 2 - 70,
-          g = svg.append('g').attr('transform', 'translate(' + 200 + ',' + height / 2 + ')')
-    
-        svg.append('text')
-          .attr('id', 'chart-title')
-          .text('Testy mctesttest')
-          .style('font-size', 24)
-          .style('font-weight', 'bold')
-          .attr('fill', '#e1e1e1')
-          .attr('transform', 'translate(' + 400 + ',' + 35 + ')')
+        var canvas = d3.select('#prims-canvas'),
+        context = canvas.node().getContext("2d"),
+        width = canvas.property("width"),
+        height = canvas.property("height");
+
+        var colors = d3.range(360)
+            .map((function() {
+            var color = d3.scaleLinear()
+                .domain([0, 180, 360])
+                .range([
+                d3.hsl(-100, 0.75, 0.35),
+                d3.hsl(  80, 1.50, 0.80),
+                d3.hsl( 260, 0.75, 0.35)
+                ]);
+            return function(i) {
+                return d3.rgb(color(i));
+            };
+        })());
+
+        var worker = new Worker("/generate-prims.js");
+        worker.postMessage({width: width, height: height});
+        worker.addEventListener("message", function(event) {
+            worker.terminate();
+
+            var N = 1 << 0,
+                S = 1 << 1,
+                W = 1 << 2,
+                E = 1 << 3;
+
+            var d = 0,
+                n = width * height,
+                cells = event.data,
+                distance = new Array(n),
+                frontier = [(height - 1) * width],
+                image = context.createImageData(width, height),
+                imageData = image.data;
+
+            distance[frontier[0]] = 0;
+
+            for (var i = 0, i4 = 3; i < n; ++i, i4 += 4) {
+                imageData[i4] = 255;
+            }
+
+            while (frontier.length) {
+                let frontier1 = [];
+                let i0 = 0;
+                let n0 = frontier.length;
+                let i1 = 0;
+
+                ++d;
+                for (var i = 0; i < n0; ++i) {
+                    i0 = frontier[i];
+                    if (cells[i0] & E && distance[i1 = i0 + 1] == null) {
+                        distance[i1] = d;
+                        frontier1.push(i1);
+                    }
+                    if (cells[i0] & W && distance[i1 = i0 - 1] == null) {
+                        distance[i1] = d; 
+                        frontier1.push(i1);
+                    };
+                    if (cells[i0] & S && distance[i1 = i0 + width] == null) {
+                        distance[i1] = d; 
+                        frontier1.push(i1);
+                    } 
+                    if (cells[i0] & N && distance[i1 = i0 - width] == null) {
+                        distance[i1] = d; 
+                        frontier1.push(i1);
+                    } 
+                }
+
+                frontier = frontier1;
+            }
+
+            d3.timer(function(elapsed) {
+                for (var i = 0, c, r = Math.floor(elapsed / 20), i4 = 0; i < n; ++i, i4 += 4) {
+                c = colors[(c = (distance[i] - r) % 360) < 0 ? c + 360 : c];
+                imageData[i4] = c.r;
+                imageData[i4 + 1] = c.g;
+                imageData[i4 + 2] = c.b;
+                }
+                context.putImageData(image, 0, 0);
+            })
+        })
     }
-    
+
     _setRef(componentNode) {
         this._rootNode = componentNode
     }
@@ -32,17 +102,12 @@ class Sample extends React.Component {
     render() {
         return (
           <div>
-            <svg
-              className="sample-d3"
-              width="876"
-              height="460"
-              fontFamily="PT Sans"
-              fontSize="11"
-              fill='#242424'
-              ref={this._setRef.bind(this)}
-              preserveAspectRatio="xMidYMid meet"
-              viewBox="0 0 876 460"
-            />
+            <canvas
+                id="prims-canvas"
+                ref={this._setRef.bind(this)} 
+                width="960"
+                height="500">
+            </canvas>
           </div>
         )
     }
